da40d87d878a3618e7709765b49f46a349df5d42
1324554 <studyinq13@gmail.com>
2025-05-09 21:07:07 +0900

    Update data.py
    키 수정

--- data.py
+++ data.py
@@ -2,7 +2,7 @@
 import requests  # requests 모듈 임포트
 
 # URL과 저장 경로 변수를 지정합니다.
-url = f'https://apihub.kma.go.kr/api/typ01/url/kma_sfctm2.php?&stn=131&help=1&authKey=lCL7eoqyTzGi-3qKst8xEQ'
+url = f'https://apihub.kma.go.kr/api/typ01/url/kma_sfctm2.php?&stn=131&help=1&authKey=xxxxxxxxxxxxxxxx'
 save_path = './OpenSourceBasicProj_Ass/teamproj/output_file.txt'
 
 try:
